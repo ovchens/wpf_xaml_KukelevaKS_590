f15b0c266a1fcdba0b2d89e360135cbeb1503d30
Your Name <your_email@whatever.com>
2021-09-26 20:56:08 +0300

    Добавление проектов

---
+++ wpf_lesson_1/_8Ball/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _8Ball
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string GetRandomAnswer()
+        {   
+            string[] answer = { "Вероятно", 
+                                    "Мало вероятно", 
+                                            "Однозначно да", 
+                                                    "Однозначно нет",
+                                                               "Скорее нет" };
+
+            Random  r = new Random();
+         
+                int a = r.Next(4);
+                return answer[a];
+
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtQuestion_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Answer_Click(object sender, RoutedEventArgs e)
+        {
+            this.Cursor = Cursors.Wait;
+            // Делаем задержку, для создания эффекта того, что программа задумалась.
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            // Берем случайный ответ.
+            txtAnswer.Text = GetRandomAnswer();
+            // Восстанавливаем прежнее состояние курсора.
+            this.Cursor = null;
+
+        }
+    }
+}
---
+++ wpf_lesson_1/копия/Установка свойств в разметке/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Установка_свойств_в_разметке
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
